26a7bd0d248cd427ac5ef4afc64243cb5cb25de2
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-02-16 22:04:24 +0100

    1.0.2

--- src/picolog.js
+++ src/picolog.js
@@ -1,10 +1,10 @@
 var log = {NONE:0, ERROR:1, WARN:2, INFO:3, LOG:4, DEBUG:5, TRACE:6},
 	p=typeof process=='object' && process, prod = p && p.env.NODE_ENV === 'production',
 	k=Object.keys(log), q=qry(), c=con(), env=p && lvl(p.env.PICOLOG_LEVEL),
-	level = q !== undefined ? q : (env !== false ? env : log.WARN),
+	level = typeof q != 'undefined' ? q : (env !== false ? env : log.WARN),
 	names=k.slice(1, k.length).map(function(x){return x.toLowerCase();});
 
-Object.defineProperty(log, 'picolog', {configurable:false, enumerable:false, value:{version:'1.0.1'}});
+Object.defineProperty(log, 'picolog', {configurable:false, enumerable:false, value:{version:'1.0.2'}});
 
 Object.defineProperty(log, 'level', {
 	get: function(){return level;},
@@ -29,7 +29,7 @@ function nop(){}
 function bnd(n){return c && (c[n]||c.log).bind(c);}
 function con(){return typeof console=='object' && console;}
 function prn(){return typeof print == 'function' && print;}
-function lvl(n){return typeof n == 'string' ? log[n.toUpperCase()] : n;}
+function lvl(n){return Number(n)!==Number(n) ? log[n.toUpperCase()] : Number(n);}
 function qry(){
 	var m, qs = typeof window=='object' && window.location.search.substring(1);
 	for (; (m = qs && /([^&=]+)=?([^&]*)/g.exec(qs)) ;) {
